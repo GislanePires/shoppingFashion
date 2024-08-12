e3d1d190278d0f836dd23a38c8d59928d24124a6
gislanepires <piresgislane7@gmail.com>
2024-08-12 16:58:20 -0300

    página catalogue em andamento

--- src/pages/Catalogue/index.jsx
+++ src/pages/Catalogue/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-// import catalogue1 from "./pages/";
+import { GoArrowRight } from "react-icons/go";
+import catalogue1 from "../../assets/images/catalogue1.png";
+import catalogue2 from "../../assets/images/catalogue2.png";
+import catalogue3 from "../../assets/images/catalogue3.png";
 
 const Catalogue = () =>{
     return(
@@ -7,7 +10,20 @@ const Catalogue = () =>{
             <section>
                 <h2>NEW ARRIVALS</h2>
                 <figure>
-                    {/* <img src={catalogue1} /> */}
+                    <img src={catalogue1} />
+                    <figcaption>Hoodies & Sweetshirt</figcaption>
+                    <figcaption class="explore-link"><a href="#">Explore Now!<GoArrowRight /></a></figcaption>                </figure>
+                <figure>
+                    <img src={catalogue2} />
+                    <figcaption>Coats & Parkas</figcaption>
+                    <figcaption class="explore-link"><a href="#">Explore Now!<GoArrowRight /></a></figcaption>
+                </figure>
+                <figure>
+                    <img src={catalogue3} />
+                    <figcaption>Tees & T-Shirt</figcaption>
+                    <figcaption class="explore-link"><a href="#">Explore Now!<GoArrowRight /></a></figcaption>
+                    
+
                 </figure>
             </section>
 
